6fa338036690ca4f2644802790ca0999f8e72675
cozm7c <gotenmcdermott007@gmail.com>
2025-08-22 10:55:52 -0400

    gabriel HelloWorld_Version4.java

--- HelloWorld_Version4.java
+++ HelloWorld_Version4.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         // Change the text below to include your name and GitHub username
-        System.out.println("Hello, World! My name is <Your Name> and my GitHub username is <your_username>.");
+        System.out.println("Hello, World! My name is Gabriel and my GitHub username is Cozm7c :3.");
     }
 }
